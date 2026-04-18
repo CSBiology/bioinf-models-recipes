550096e3e8eaf87b190321a9aa0635651e6bc4dc
muehlhaus <timo.muehlhaus@rptu.de>
2026-04-18 16:55:39 +0200

    Fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,28 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: 'https://CSBiology.github.io/bioinf-models-recipes/',
+  integrations: [
+    starlight({
+      title: 'Bioinformatics - Models and Recipes',
+      social: [
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/CSBiology/bioinf-models-recipes' },
+      ],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            { label: 'Introduction', slug: 'guides/introduction' },
+            { label: 'Modeling Biological Systems', slug: 'guides/modeling-biological-systems' },
+            { label: 'Recipes', slug: 'guides/recipes' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
 });
